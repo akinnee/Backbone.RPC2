475a1fd481d29074e8af0ca9d48050829c461f27
Alex Kinnee <akinnee@barracuda.com>
2014-02-09 13:53:46 -0500

    Created plugin file with a jQuery JSON RPC client submodule

---
+++ backbone.rpc2.js
@@ -0,0 +1,21 @@
+/**
+ * RPC2
+ * A Backbone plugin that allows Models and Collections to be retrieved and updated over JSON RPC 2.0
+ * instead of REST
+ */
+(function () {
+
+	// Define the RPC2 plugin
+	var RPC2 = {};
+
+	// Attach our plugin to Backbone
+	Backbone.RPC2 = RPC2;
+
+	// Add a new type of Model
+	RPC2.Model = Backbone.Model.extend({
+
+
+
+	});
+
+}());
