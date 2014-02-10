cd07d10fa472bf94333b32343f26582661d73561
Alex Kinnee <akinnee@barracuda.com>
2014-02-10 17:00:57 -0500

    Break out our sync method

--- backbone.rpc2.js
+++ backbone.rpc2.js
@@ -24,7 +24,51 @@ if (typeof $.toJSON === 'undefined') {
 	// Attach our plugin to Backbone
 	Backbone.RPC2 = RPC2;
 
-	// Add a new type of Model
+	/**
+	 * A custom sync method to use JSON RPC
+	 * @param  {[string]} method – the CRUD method ("create", "read", "update", or "delete")
+	 * @param  {[object]} model – the model to be saved (or collection to be read)
+	 * @param  {[type]} options – success and error callbacks, and all other jQuery request options
+	 */
+	RPC2.sync = function(method, model, options) {
+
+		var client = new $.JsonRpcClient({
+			ajaxUrl: model.url
+		});
+
+		var success = function(response) {
+
+			// call the success callback
+			options.success(model, response, options);
+
+		};
+
+		var error = function(response) {
+
+			// call the error callback
+			options.error(model, response, options);
+
+		};
+
+		if (method == "create") {
+			client.call(model.rpcOptions.methods.create.method, model.constructParams("create"), success, error);
+
+		} else if (method == "read") {
+			client.call(model.rpcOptions.methods.read.method, model.constructParams("read"), success, error);
+
+		} else if (method == "update") {
+			client.call(model.rpcOptions.methods.update.method, model.constructParams("update"), success, error);
+
+		} else if (method == "delete") {
+			client.call(model.rpcOptions.methods.delete.method, model.constructParams("delete"), success, error);
+		}
+
+	};
+
+	/**
+	 * Backbone.RPC2.Model
+	 * An extension of Backbone.Model which uses RPC2.sync instead of the default Backbone.sync
+	 */
 	RPC2.Model = Backbone.Model.extend({
 
 		/**
@@ -71,7 +115,9 @@ if (typeof $.toJSON === 'undefined') {
 
 			$.each(this.rpcOptions.methods[method].params, function(param, attribute) {
 
-				params[param] = model.get(attribute);
+				if (model.get(attribute)) {
+					params[param] = model.get(attribute);
+				}
 
 			});
 
@@ -79,49 +125,11 @@ if (typeof $.toJSON === 'undefined') {
 
 		},
 
-		parse: function(response) {
-			return response;
-		},
-
 		/**
-		 * A custom sync method to use JSON RPC
-		 * @param  {[string]} method – the CRUD method ("create", "read", "update", or "delete")
-		 * @param  {[object]} model – the model to be saved (or collection to be read)
-		 * @param  {[type]} options – success and error callbacks, and all other jQuery request options
+		 * Maps to our custom sync method
 		 */
 		sync: function(method, model, options) {
-
-			var client = new $.JsonRpcClient({
-				ajaxUrl: model.url
-			});
-
-			var success = function(response) {
-
-				// call the success callback
-				options.success(model, response, options);
-
-			};
-
-			var error = function(response) {
-
-				// call the error callback
-				options.error(model, response, options);
-
-			};
-
-			if (method == "create") {
-				client.call(model.rpcOptions.methods.create.method, model.constructParams("create"), success, error);
-
-			} else if (method == "read") {
-				client.call(model.rpcOptions.methods.read.method, model.constructParams("read"), success, error);
-
-			} else if (method == "update") {
-				client.call(model.rpcOptions.methods.update.method, model.constructParams("update"), success, error);
-
-			} else if (method == "delete") {
-				client.call(model.rpcOptions.methods.delete.method, model.constructParams("delete"), success, error);
-			}
-
+			return RPC2.sync(method, model, options);
 		}
 
 	});
