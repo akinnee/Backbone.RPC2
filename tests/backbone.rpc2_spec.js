a37eba2b92523e69bc38121f48e6234b27166c99
Alex Kinnee <akinnee@barracuda.com>
2014-02-19 13:28:15 -0500

    added some more basic tests

--- tests/backbone.rpc2_spec.js
+++ tests/backbone.rpc2_spec.js
@@ -1,8 +1,30 @@
 $(function() {
 	describe("Backbone.RPC2", function() {
 
+		var model;
+
+		beforeEach(function() {
+			model = new Backbone.RPC2.Model();
+		});
+
 		it("adds the RPC2 object to the Backbone object", function() {
 			expect(Backbone.RPC2).toBeDefined();
+		});
+
+		it("defines a custom sync method", function() {
+			expect(Backbone.RPC2.sync).toBeDefined();
+		});
+
+		it("adds a new kind of model", function() {
+			expect(Backbone.RPC2.Model).toBeDefined();
+		});
+
+		it("adds a new kind of collection", function() {
+			expect(Backbone.RPC2.Collection).toBeDefined();
+		});
+
+		it("uses the custom sync method for the new model", function() {
+			expect(model.sync).toEqual(Backbone.RPC2.sync);
 		});
 
 	});
